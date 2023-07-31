d1e448dc60f97da09303162bca6f10863e6a6f1a
Shankarsingla <51204511+Shankarsingla@users.noreply.github.com>
2023-07-31 18:53:48 +0530

    Add files via upload

--- add_form.js
+++ add_form.js
@@ -1,3 +1,4 @@
+
 function showToast(message) {
     const toast = document.getElementById('toast');
     const toastMessage = document.getElementById('toastMessage');
@@ -6,47 +7,49 @@ function showToast(message) {
     toast.classList.add('show');
     setTimeout(() => {
         toast.classList.remove('show');
-    }, 8000); 
+    }, 8000);
 }
-function canceladd()
-{
+function dataURLtoBlob(dataURL) {
+  var arr = dataURL.split(','), mime = arr[0].match(/:(.*?);/)[1];
+  var bstr = atob(arr[1]);
+  var n = bstr.length;
+  var u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], { type: mime });
+}
+
+function canceladd() {
     window.location.href = "admin_dashboard.php";
 }
-function previewImage1(event) {
-    const input = event.target;
-    if (input.files && input.files[0]) {
-    const reader = new FileReader();
-    reader.onload = function(e) {
-    const preview = document.getElementById('preview1');
-    preview.src = e.target.result;
-    preview.style.display = 'block';
-    };
-    reader.readAsDataURL(input.files[0]);
-    }
-    }
+
 $(document).ready(function() {
-    $("#add-form-data").on("submit",function(e)
-    {
-      e.preventDefault();
-        var formdata=new FormData(this);
+    $("#add-form-data").on("submit", function(e) {
+        e.preventDefault();
+        var formdata = new FormData(this);
+        var croppedImageSrc = document.getElementById('cropped_image').value;
+    var croppedImageBlob = dataURLtoBlob(croppedImageSrc);
+    var fileName = "cropped_image.png";
+    var croppedImageFile = new File([croppedImageBlob], fileName);
+    formdata.append('uploaded_file', croppedImageFile);
         $.ajax({
-                url:'add.php', 
-                type: "POST",
-                data: formdata,
-                 processData: false,
-                  contentType: false,
-                  success: function(response) {
-                
-                     showToast(response);
-                     setTimeout(function() {
-                        window.location.href = "admin_dashboard.php";
-                    }, 1500); 
-                    //window.location.href = "admin_dashboard.php";
-           },
-           error: function() {
-               showToast("Server error. Please try again later.");
-           }
-       });
-   });
+            url: 'add.php',
+            type: "POST",
+            data: formdata,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                showToast(response);
+                setTimeout(function() {
+                    window.location.href = "admin_dashboard.php";
+                }, 1500);
+                //window.location.href = "admin_dashboard.php";
+            },
+            error: function() {
+                showToast("Server error. Please try again later.");
+            }
+        });
+    });
 });
-    
+
--- add_form.php
+++ add_form.php
@@ -12,6 +12,11 @@ if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ADD Record</title>
+    <script src="https://jsuites.net/v4/jsuites.js"></script>
+<link rel="stylesheet" href="https://jsuites.net/v4/jsuites.css" type="text/css" />
+<script src="https://cdn.jsdelivr.net/npm/@jsuites/cropper/cropper.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@jsuites/cropper/cropper.min.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css">
     <link rel="stylesheet" href="add_form.css">
       
 </head>
@@ -19,29 +24,48 @@ if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
 <div class="toast" id="toast">
         <div class="toast-message" id="toastMessage"></div>
     </div>   
-<div id="add-form">
-              <h2>ADD Intern Details</h2>
-              
-              <form id="add-form-data" method="post">
-                <label> Photo</label>
-                <input type="file" name="uploaded_file" id="image" onchange="previewImage1(event)"> <br> <br>
-                <label>Name:</label>
-                <input type="text" id="addname" value="" name="Name" required><br><br>
-                <label>Email:</label>
-                <input type="text" id="addemail" value="" name="Email" required><br><br>
-                <label>Gender:</label>
-                <input type="radio" value="Male" name="Gender">Male
-                <input type="radio" value="Female" name="Gender">Female
-                <input type="radio" value="Other" name="Gender">Other
-                <br><br>
-                <input type="submit" class="submit" value="ADD">
-                <button type="button" onclick="canceladd()">Cancel</button><br><br>
-                <img id="preview1" src="#" alt="Preview Image" style="display: none; max-width: 200px; max-height: 200px;">
-              </form>
-              
+    <div id="add-form">
+        <h2>ADD Intern Details</h2>
+        <form id="add-form-data" method="post">
+            <div style="display: flex;">
+                <div id="image-cropper" style="border:1px solid #ccc; margin: 5px;"></div>
+                <div id="image-cropper-result"><img style="width:120px; height:120px; margin: 5px;"></div>
             </div>
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-            <script src="add_form.js"></script>
-            </script>
+            <p><input type="button" value="Get cropped image" id="image-getter" class="jbutton dark"></p>
+            <input type="hidden" name="cropped_image" id="cropped_image">
+            <input type="file" name="uploaded_file" id="image" accept="image/*" style="position: absolute; top: -9999px;">
+            <label>Name:</label>
+            <input type="text" id="addname" value="" name="Name" required><br><br>
+            <label>Email:</label>
+            <input type="text" id="addemail" value="" name="Email" required><br><br>
+            <label>Gender:</label>
+            <input type="radio" value="Male" name="Gender">Male
+            <input type="radio" value="Female" name="Gender">Female
+            <input type="radio" value="Other" name="Gender">Other
+            <br><br>
+            <input type="submit" class="submit" value="ADD">
+            <button type="button" onclick="canceladd()">Cancel</button><br><br>
+            <img id="preview1" src="#" alt="Preview Image" style="display: none; max-width: 200px; max-height: 200px;">
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js"></script>
+    <script src="add_form.js"></script>
+    <script>
+        cropper(document.getElementById('image-cropper'), {
+            area: [200, 200],
+            crop: [100, 100],
+        });
+
+        document.getElementById('image-getter').onclick = function() {
+            var x = document.getElementById('image-cropper-result');
+            var croppedImageSrc = document.getElementById('image-cropper').crop.getCroppedImage().src;
+
+            x.children[0].src = croppedImageSrc;
+            document.getElementById('cropped_image').value = croppedImageSrc;
+        };
+</script>
+
 </body>
 </html>
